3383770fd96542e6a801d68cecdb5a0976f09489
Wilovy09 <srwilovy09@gmail.com>
2024-10-30 18:23:57 -0600

    :bug: Se arregla error al hacer la busqueda de producto por código

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -15,7 +15,7 @@
 
   let codigoUser = "";
   async function searchCode() {
-    const url = `http://localhost:8000/api/product/name/${codigoUser}`;
+    const url = `http://localhost:8000/api/product/code/${codigoUser}`;
     const options = {
       method: "GET",
       headers: {
